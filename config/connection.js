aee8d086399295e5b1d232a1dd9db74e94481ba2
xiaolu sun <xiaolusun@xiaolus-MBP.fios-router.home>
2020-05-06 11:59:06 -0400

    add JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,4 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,8 +6,18 @@ var connection = mysql.createConnection({
   password: "",
   database: "burger"
 });
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection =mysql.createConnection({
+    hose: 'localhost',
+    user: 'roor',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +26,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
