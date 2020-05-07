8f4ebe3fd99c39a4b8147536d9ef3bca074cc959
xiaolu sun <xiaolusun@xiaolus-MacBook-Pro.local>
2020-05-06 22:11:52 -0400

    add config.json

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "roottec0wcaaysn0yzp3",
-  password: "cfs4q2ef6tau7w7n",
-  database: "burger"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burger"
+// });
 if (process.env.JAWSDB_URL){
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection =mysql.createConnection({
-    hose: 'localhost',
-    user: 'roor',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
+  var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burger"
   });
 };
 
